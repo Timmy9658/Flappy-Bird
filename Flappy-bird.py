4088c640b7a90d802085d7f9b5b4fe5a9f977883
Timmy9658 <t.yulius197752@simivalleyusd.org>
2024-03-09 10:33:53 -0800

    Movement & Image showing
    We started making the image and the movement of it

--- Flappy-bird.py
+++ Flappy-bird.py
@@ -1,13 +1,23 @@
 import pygame
 import time
-
-window_height, window_width = 700, 700
-class space(pygame.sprite.Sprite):
-    def init(self):
-        super().init()
-        self.image = pygame.image.load("Flappy.png")
-
-pygame.display.set_mode((window_height, window_width))
-bird = space()
-time.sleep(1)
 pygame.init()
+window_height, window_width = 700, 700
+player_x, player_y = 200,200
+game = True
+class space(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Flappy.png")
+        print(self.image)
+        self.rect = self.image.get_rect()
+    def movement(self):
+        self.rect.y += 1
+screen = pygame.display.set_mode((window_height, window_width))
+screen.fill((0,0,0))
+bird = space()
+screen.blit(bird.image, (player_x,player_y))
+while game == True:
+    bird.movement()
+    screen.blit(bird.image, (player_x, bird.rect.y))
+    pygame.display.update()
+    pygame.time.Clock().tick(60)
